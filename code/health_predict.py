9757a8f4286a3ddd856eb106eb44caf2ae05b114
rockey2018 <15295750221@139.com>
2018-06-14 17:59:25 +0800

    commit update at 20180614

--- code/health_predict.py
+++ code/health_predict.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[34]:
+# In[101]:
 
 
 import numpy as np
@@ -12,7 +12,9 @@ import os
 import matplotlib.pyplot as plt
 import sys
 from keras.utils.vis_utils import plot_model
+from IPython.display import SVG
 import time
+from keras.callbacks import Callback, ModelCheckpoint
 
 
 # In[35]:
@@ -22,49 +24,28 @@ timesteps = seq_length = 10
 data_dim = 50
 
 
-# In[36]:
+# In[123]:
 
 
 xy = np.loadtxt('./data/health_161.csv', delimiter=',',skiprows=1)
-type(xy)
 
 
-# In[38]:
-
-
-xy.shape[1]
-
-
-# In[84]:
-
-
-#xy = xy[::-1]
-
-
-# In[39]:
-
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-xy = scaler.fit_transform(xy)
-
-
-# In[40]:
+# In[124]:
 
 
 x = xy[:,8:xy.shape[1]]
 y = xy[:, 0]
 
 
-# In[33]:
+# In[125]:
 
 
-#xy[1,1:len(xy[1,:])]
-#type(x)
-x.shape
-#y[0]
+scaler = MinMaxScaler(feature_range=(0, 1))
+x = scaler.fit_transform(x)
+y = y/100
 
 
-# In[41]:
+# In[129]:
 
 
 dataX = []
@@ -77,13 +58,7 @@ for i in range(0, len(y) - seq_length):
     dataY.append(_y)
 
 
-# In[43]:
-
-
-len(dataX)
-
-
-# In[44]:
+# In[130]:
 
 
 train_size = int(len(dataY) * 0.6)
@@ -93,12 +68,6 @@ trainX, validX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size
     dataX[train_size+valid_size:len(dataX)])
 trainY, validY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:train_size+valid_size]),np.array(
     dataY[train_size+valid_size:len(dataY)])
-
-
-# In[32]:
-
-
-validX
 
 
 # model = Sequential()
@@ -112,31 +81,48 @@ validX
 # #plot_model(model, to_file='model.png', show_shapes=True)
 # print(trainX.shape, trainY.shape)
 
-# In[45]:
+# In[131]:
 
 
 model = Sequential()
 model.add(LSTM(32, input_shape=(timesteps, data_dim), return_sequences=True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(LSTM(8, return_sequences=True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(LSTM(2, return_sequences=False))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(8,activation = 'relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(1))
 model.add(Activation('linear'))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.summary()
 
 
-# In[49]:
+# In[132]:
 
 
-model.fit(trainX, trainY, epochs=100, verbose=2, batch_size=32, validation_data=(validX, validY))
+#记录损失函数的历史数据
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+history = LossHistory()
+
+#如果验证损失下降， 那么在每个训练轮之后保存模型。
+checkpointer = ModelCheckpoint(filepath='./models/health_check.hdf5', verbose=1, save_best_only=True)
 
 
-# In[50]:
+# In[133]:
+
+
+model.fit(trainX, trainY, epochs=200, verbose=1, batch_size=32, 
+          validation_data=(validX, validY), callbacks=[history, checkpointer])
+
+
+# In[134]:
 
 
 #model.save('./models/health_'+ time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime()) + '.h5')
@@ -144,67 +130,43 @@ model.save('./models/health.h5')
 #plot_model(model, to_file='./models/model.png', show_shapes=True, show_layer_names=True)
 
 
-# In[52]:
+# In[135]:
+
+
+losses = history.losses
+print ("history: ",np.mean(losses))
+#SVG(plot_model(model, to_file='./models/model.png', show_shapes=True, show_layer_names=True).create(prog='dot', format='svg'))
+
+
+# In[136]:
 
 
 loadModel = load_model('./models/health.h5')
 #loss = model.evaluate(x=testX, y=testY, batch_size=2)
 
 
-# In[65]:
-
-
-a_testX = testX[0].reshape(1,10,50)
-a_testY = testY[0]
-
-
-# In[63]:
-
-
-a_testPredict = loadModel.predict(a_testX)
-
-
-# In[67]:
-
-
-print (a_testPredict)
-print (a_testY)
-
-
-# In[68]:
+# In[137]:
 
 
 testPredict = loadModel.predict(testX)
 
 
-# In[54]:
+# In[155]:
 
 
-print (testPredict)
+for i in range(0,len(testY)):
+    print (testPredict[i],"-->",testY[i])
 
 
-# In[55]:
+# In[156]:
 
 
-plt.figure(figsize=(12,6))
-plt.plot(testY,c='r',label="train")
-plt.plot(testPredict,c='g',label="predict")
+plt.figure(figsize=(24,6))
+plt.plot(testY[0:200],c='r',label="train")
+plt.plot(testPredict[0:200],c='g',label="predict")
 plt.xlabel("time aix")
 plt.ylabel("value aix")
 plt.legend() 
 plt.show()
 plt.savefig('reslut.jpg')
-
-
-# In[21]:
-
-
-print (model.layers)
-
-
-# In[50]:
-
-
-import time
-print ("local time is "+time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime()))
 
